cf6c68a021223451e2bf524b55e4242b2b10a1f6
neerajkumar161 <ennkay161@gmail.com>
2023-05-11 12:05:47 +0530

    Section 10: Safety Handling Untrusted code exec

--- jbook-demo-third/src/index.tsx
+++ jbook-demo-third/src/index.tsx
@@ -6,8 +6,8 @@ import { unpkgPathPlugin } from './plugins/unkpg-path-plugin'
 
 const RootApp = () => {
   const ref = useRef<esbuild.Service>()
+  const iFrameRef = useRef<any>() 
   const [input, setInput] = useState<string>('')
-  const [code, setCode] = useState<string>('')
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -19,8 +19,9 @@ const RootApp = () => {
   const onSubmit = async () => {
     if (!ref.current) return
 
+    iFrameRef.current!.srcdoc = html
     console.log(ref.current)
-    // const result = await ref.current.transform(input, { loader: 'jsx', target: 'es2015' })
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -29,21 +30,43 @@ const RootApp = () => {
       define: { 'process.env.NODE_ENV': '"production"', global: 'window' }
     })
     console.log({ result })
-
-    setCode(result.outputFiles[0].text)
+    if(iFrameRef.current) {
+      iFrameRef.current.contentWindow!.postMessage(result.outputFiles[0].text, '*')
+    }
   }
 
   useEffect(() => {
     startService()
   }, [])
 
+  const html = `
+  <html>
+  <head></head>
+  <body>
+    <div id="root"></div>
+    <script>
+      window.addEventListener('message', (event) => {
+        try {
+          eval(event.data)
+        } catch (error) {
+          const rootEl = document.querySelector('#root')
+          rootEl.innerHTML = '<div style="color: red">Error Occured' + error + '</div>'
+          console.error(error)
+          throw err
+        }
+      }, false)
+    </script>
+  </body>
+  </html>
+  `
+  
   return (
     <div>
       <textarea name='' id='' cols={30} rows={10} onChange={(e) => setInput(e.target.value)} value={input}></textarea>
       <div>
         <button onClick={onSubmit}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe ref={iFrameRef} srcDoc={html} sandbox='allow-scripts' title='iFrame' />
     </div>
   )
 }
